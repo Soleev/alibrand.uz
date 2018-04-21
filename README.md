fe89904ed9ffd84e9b12a1481b54380d85bfdcaa
Шохрух <shohruh@yandex.ru>
2018-04-22 00:29:57 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+alibrand.uz
---
+++ admin/language/ru-ru/common/footer.php
@@ -0,0 +1,5 @@
+<?php
+// Text
+$_['text_footer']  = '<HR><a href="http://www.soleyev.uz">soleyev.uz</a> &copy; 2000-' . date('Y') . ' Все права защищены.<br> <BR> <a href="http://www.soleyev.uz" target="_blank">soleyev.uz</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="http://forum.soleyev.uz" target="_blank">Форум поддержки</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="http://www.soleyev.uz" target="_blank">Магазин дополнений</a>';
+$_['text_version'] = 'Version %s (rs.3)';
+
